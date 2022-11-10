bd108daa547f6e533097b98ff09492aee6d40fbb
Avneesh002 <Avneeshgrover8@gmail.com>
2022-11-10 21:08:06 +0530

    checkout page

---
+++ checkout.js
@@ -0,0 +1,16 @@
+
+
+let returnToCart = () => {
+    location.href = 'cart.html' 
+}
+
+document.getElementById('pyoBtn').addEventListener('click', function(){
+
+    let firstName = document.getElementById('firstName').value;
+
+    let lastName = document.getElementById('lastName').value;
+
+
+    
+
+})
